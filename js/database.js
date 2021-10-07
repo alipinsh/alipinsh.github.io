8306f86edd41a2b9ef583c9db67d142e6b44caed
alipinsh <a.lipinsh@gmail.com>
2021-10-08 01:07:22 +0300

    fixes

---
+++ js/database.js
@@ -0,0 +1,14 @@
+let comics = [
+    {
+        title: "Лес",
+        chapters: [
+            { heading: "Глава 1 - Неспокойное дерево", pages: 34 },
+            { heading: "", pages: 32 },
+            { heading: "", pages: 32 },
+            { heading: "", pages: 32 },
+            { heading: "", pages: 32 },
+            { heading: "", pages: 32 },
+            { heading: "", pages: 32 }
+        ]
+    }
+];
--- js/reader.js
+++ js/reader.js
@@ -1,9 +1,3 @@
-"use strict";
-
-let pages = [
-	[34, 32, 32, 32, 32, 32, 32]
-];
-
 let comic = 0;
 let chapter = 0;
 let page = 0;
@@ -12,116 +6,155 @@ let imgContainer;
 let firstLoad = 5;
 
 function changeContent(direction) {
-	imgContainer.children[page-1-direction].style.display = "none";
-	imgContainer.children[page-1].style.display = "inline";
+    imgContainer.children[page-1-direction].style.display = 'none';
+    imgContainer.children[page-1].style.display = 'block';
 }
 
 function getCurrentParams() {
-	let params = new URLSearchParams(location.search);
-	comic = Number(params.get('s'));
-	chapter = Number(params.get('c'));
-	page = Number(params.get('p'));
+    let params = new URLSearchParams(location.search);
+    comic = Number(params.get('s'));
+    chapter = Number(params.get('c'));
+    page = Number(params.get('p'));
 }
 
 function loadPage(p) {
-	let pageElement = imgContainer.children[p-1];
-	if (pageElement.getAttribute("data-src")) {
-		pageElement.setAttribute("src", pageElement.getAttribute("data-src"));
-		pageElement.removeAttribute("data-src");
-	}
+    let pageElement = imgContainer.children[p-1];
+    if (pageElement.getAttribute('data-src')) {
+        pageElement.setAttribute('src', pageElement.getAttribute('data-src'));
+        pageElement.removeAttribute('data-src');
+    }
 }
 
 function checkParams() {
-	if (pages[comic-1] != undefined) {
-		let pageCount = pages[comic-1][chapter-1];
-		if (pageCount != undefined) {
-			if (page > 0 && page <= pageCount) {
-				return true;
-			}
-		}
-	}
-	
-	return false;
+    if (comics[comic-1] != undefined) {
+        let selectedChapter = comics[comic-1]['chapters'][chapter-1];
+        if (selectedChapter != undefined) {
+            let pageCount = selectedChapter['pages'];
+            if (pageCount != undefined) {
+                if (page > 0 && page <= pageCount) {
+                    return true;
+                }
+            }
+        }
+    }
+    
+    return false;
 }
 
 function firstLoadContent() {
-	imgContainer = document.querySelector(".imageContainer");
-	
-	for (let i = 1; i <= pages[comic-1][chapter-1]; ++i) {
-		let imgElement = document.createElement("img");
-		imgElement.setAttribute("class", "comicPage");
-		imgElement.setAttribute("data-src", `img/comics/${comic}/${chapter}/${i}.png`);
-		imgElement.style.display = 'none';
-		imgContainer.appendChild(imgElement);
-	}
-	
-	for (let i = 0; i < firstLoad; ++i) {
-		if (page+i <= pages[comic-1][chapter-1]) {
-			loadPage(page+i);
-		}
-	}
-	
-	imgContainer.children[page-1].style.display = "inline";
+    imgContainer = document.querySelector('.image-container');
+    
+    document.querySelector('.bottom h1').innerText = comics[comic-1]['title'];
+    document.querySelector('.bottom h2').innerText = comics[comic-1]['chapters'][chapter-1]['heading'];
+    
+    let pageCount = comics[comic-1]['chapters'][chapter-1]['pages'];
+    for (let i = 1; i <= pageCount; ++i) {
+        let imgElement = document.createElement('img');
+        imgElement.setAttribute('class', 'page-image');
+        imgElement.setAttribute('data-src', `img/comics/${comic}/${chapter}/${i}.png`);
+        imgElement.style.display = 'none';
+        imgContainer.appendChild(imgElement);
+    }
+    
+    for (let i = 0; i < firstLoad; ++i) {
+        if (page+i <= pageCount) {
+            loadPage(page+i);
+        }
+    }
+    
+    imgContainer.children[page-1].style.display = 'block';
 }
 
 function postLoadContent() {
-	let pageToLoad = page + firstLoad - 1;
-	if (pageToLoad <= pages[comic-1][chapter-1]) {
-		loadPage(pageToLoad);
-	}
+    let pageToLoad = page + firstLoad - 1;
+    if (pageToLoad <= comics[comic-1]['chapters'][chapter-1]['pages']) {
+        loadPage(pageToLoad);
+    }
 }
 
-window.onload = function() {
-	getCurrentParams();
-	if (checkParams()) {
-		firstLoadContent();
-	} else {
-		window.open("comics.html", "_self");
-	}
-};
-
-window.addEventListener("popstate", function (e) {
-	window.open("comics.html", "_self");
-});
-
 function turnChapter(direction) {
-	chapter += direction;
-	
-	if (chapter > 0 && chapter <= pages[comic-1].length) {
-		if (direction > 0) {
-			page = 1;
-		} else {
-			page = pages[comic-1][chapter-1];
-		}
-		history.pushState("", `Comic ${comic}, Chapter ${chapter}, Page ${page}`, `reader.html?s=${comic}&c=${chapter}&p=${page}`);
-		window.scroll(0, 0);
-		while (imgContainer.firstChild) {
-			imgContainer.removeChild(imgContainer.lastChild);
-		}
-		firstLoadContent();
-	} else {
-		window.open("comics.html", "_self");
-	}
-	
+    chapter += direction;
+    
+    if (chapter > 0 && chapter <= comics[comic-1]['chapters'].length) {
+        if (direction > 0) {
+            page = 1;
+        } else {
+            page = comics[comic-1]['chapters'][chapter-1]['pages'];
+        }
+        history.pushState('', `Comic ${comic}, Chapter ${chapter}, Page ${page}`, `reader.html?s=${comic}&c=${chapter}&p=${page}`);
+        window.scroll(0, 0);
+        while (imgContainer.firstChild) {
+            imgContainer.removeChild(imgContainer.lastChild);
+        }
+        firstLoadContent();
+    } else {
+        window.open('comics.html', '_self');
+    }
 }
 
 function turnPage(direction) {
-	page += direction;
-	
-	if (page <= 0) {
-		turnChapter(-1);
-	} else if (page > pages[comic-1][chapter-1]){
-		turnChapter(1);
-	} else {
-		history.pushState("", `Comic ${comic}, Chapter ${chapter}, Page ${page}`, `reader.html?s=${comic}&c=${chapter}&p=${page}`);
-		window.scroll(0, 0);
-		if (direction > 0) {
-			changeContent(direction);
-			postLoadContent();
-		} else {
-			loadPage(page);
-			changeContent(direction);
-		}
-	}
-	
+    page += direction;
+    
+    if (page <= 0) {
+        turnChapter(-1);
+    } else if (page > comics[comic-1]['chapters'][chapter-1]['pages']){
+        turnChapter(1);
+    } else {
+        history.pushState('', `Comic ${comic}, Chapter ${chapter}, Page ${page}`, `reader.html?s=${comic}&c=${chapter}&p=${page}`);
+        window.scroll(0, 0);
+        if (direction > 0) {
+            changeContent(direction);
+            postLoadContent();
+        } else {
+            loadPage(page);
+            changeContent(direction);
+        }
+    }
+    
 }
+
+function topContainerOnClick(e) {
+    if (event.clientX > document.body.clientWidth / 2) {
+        turnPage(1);
+    } else {
+        turnPage(-1);
+    }
+}
+
+let rightDown = false;
+let leftDown = false;
+
+function arrowKeyDown(e) {
+    if (!rightDown && e.keyCode == 39) {
+        rightDown = true;
+    } else if (!leftDown && e.keyCode == 37) {
+        leftDown = true;
+    }
+}
+
+function arrowKeyUp(e) {
+    if (rightDown && e.keyCode == 39) {
+        turnPage(1);
+        rightDown = false;
+    } else if (leftDown && e.keyCode == 37) {
+        turnPage(-1);
+        leftDown = false;
+    }
+}
+
+window.addEventListener('load', function() {
+    getCurrentParams();
+    if (checkParams()) {
+        firstLoadContent();
+    } else {
+        window.open('comics.html', '_self');
+    }
+    
+    document.querySelector('.top').addEventListener('click', topContainerOnClick);
+    document.addEventListener('keydown', arrowKeyDown);
+    document.addEventListener('keyup', arrowKeyUp);
+});
+
+window.addEventListener('popstate', function (e) {
+    window.open('comics.html', '_self');
+});
